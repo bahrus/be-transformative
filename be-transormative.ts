dec1456591cff6a24fa7afdc5b29bdf42643db1e
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-06 06:20:26 -0400

    ...

---
+++ be-transormative.ts
@@ -0,0 +1,7 @@
+import {XtalDecor, XtalDecorCore} from 'xtal-decor/xtal-decor.js';
+import { XtalDecorProps } from 'xtal-decor/types';
+import {CE} from 'trans-render/lib/CE.js';
+
+const ce = new CE<XtalDecorCore<Element>>({
+    
+});
