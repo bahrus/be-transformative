798d4f12c89ce3bcfa1b3713bef70c29408504d8
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-15 11:14:59 -0400

    ...

--- be-transformative2.js
+++ be-transformative2.js
@@ -1,22 +1,20 @@
-import {BeDecoratedProps, define} from 'be-decorated/be-decorated.js';
-import {BeTransformativeActions, BeTransformativeProps, BeTransformativeVirtualProps} from './types';
+import { define } from 'be-decorated/be-decorated.js';
 import { getHost } from 'trans-render/lib/getHost.js';
 import { transform } from 'trans-render/lib/transform.js';
 import { PE } from 'trans-render/lib/PE.js';
 import { SplitText } from 'trans-render/lib/SplitText.js';
 import { nudge } from 'trans-render/lib/nudge.js';
-
-export class BeTransformativeController implements BeTransformativeActions{
-    intro(proxy: Element & BeTransformativeVirtualProps, target: Element, beDecorProps: BeDecoratedProps){
-        const params = JSON.parse(proxy.getAttribute('is-' + beDecorProps.ifWantsToBe)!);
-        for(const paramKey in params){
+export class BeTransformativeController {
+    intro(proxy, target, beDecorProps) {
+        const params = JSON.parse(proxy.getAttribute('is-' + beDecorProps.ifWantsToBe));
+        for (const paramKey in params) {
             //const pram = params[paramKey];
-            const fn = (e: Event) => {
+            const fn = (e) => {
                 const pram = params[e.type];
                 let firstTime = false;
-                if(proxy.ctx === undefined){
+                if (proxy.ctx === undefined) {
                     firstTime = true;
-                    proxy.qCache = new WeakMap<Element, {[key: string]: NodeListOf<Element>}>();
+                    proxy.qCache = new WeakMap();
                     const host = getHost(proxy);
                     proxy.ctx = {
                         match: pram.initTransform || pram.transform,
@@ -36,46 +34,44 @@ export class BeTransformativeController implements BeTransformativeActions{
                             {
                                 rhsType: String,
                                 ctor: SplitText,
-                            }                                
+                            }
                         ]
                     };
                     proxy.ctx.ctx = proxy.ctx;
-                    if(!firstTime){
+                    if (!firstTime) {
                         proxy.ctx.match = pram.transform;
                     }
-                    const hostLastEvent = (<any>host).lastEvent;
-                    (<any>host).lastEvent = e;
+                    const hostLastEvent = host.lastEvent;
+                    host.lastEvent = e;
                     const target = pram.transformFromClosest !== undefined ?
                         proxy.closest(pram.transformFromClosest)
-                        : host.shadowRoot || host!;
-                    if(target === null) throw 'Could not locate target';
+                        : host.shadowRoot || host;
+                    if (target === null)
+                        throw 'Could not locate target';
                     transform(target, proxy.ctx);
-                    (<any>host).lastEvent = hostLastEvent;
+                    host.lastEvent = hostLastEvent;
                 }
             };
             proxy.addEventListener(paramKey, fn);
-            if(proxy.eventHandlers === undefined) proxy.eventHandlers = [];
-            const on = paramKey as any as keyof ElementEventMap;
-            proxy.eventHandlers.push({on, elementToObserve: proxy, fn});
+            if (proxy.eventHandlers === undefined)
+                proxy.eventHandlers = [];
+            const on = paramKey;
+            proxy.eventHandlers.push({ on, elementToObserve: proxy, fn });
             nudge(proxy);
         }
     }
-    finale(proxy: Element & BeTransformativeVirtualProps, target:Element){
+    finale(proxy, target) {
         const eventHandlers = proxy.eventHandlers;
-        for(const eh of eventHandlers){
+        for (const eh of eventHandlers) {
             eh.elementToObserve.removeEventListener(eh.on, eh.fn);
         }
     }
 }
-
-export interface BeTransformativeController extends BeTransformativeProps{}
-
 const tagName = 'be-transformative';
-
-define<BeTransformativeProps & BeDecoratedProps<BeTransformativeProps, BeTransformativeActions>, BeTransformativeActions>({
-    config:{
+define({
+    config: {
         tagName,
-        propDefaults:{
+        propDefaults: {
             upgrade: '*',
             ifWantsToBe: 'transformative',
             noParse: true,
@@ -85,7 +81,7 @@ define<BeTransformativeProps & BeDecoratedProps<BeTransformativeProps, BeTransfo
             finale: 'finale',
         }
     },
-    complexPropDefaults:{
+    complexPropDefaults: {
         controller: BeTransformativeController
     }
 });
--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,11 @@
-import {EventHandler} from 'be-decorated/types';
+import {EventHandler, BeDecoratedProps} from 'be-decorated/types';
 import {RenderContext} from 'trans-render/lib/types';
 
 export interface ITransform{
     
 }
+
+// type transformRule = {[key: ElementEventMap]: any};
 
 export interface BeTransformativeVirtualProps{
     eventHandlers: EventHandler[];
@@ -17,5 +19,6 @@ export interface BeTransformativeProps extends BeTransformativeVirtualProps{
 }
 
 export interface BeTransformativeActions{
-
+    intro(proxy: Element & BeTransformativeVirtualProps, target: Element, beDecorProps: BeDecoratedProps): void;
+    finale(proxy: Element & BeTransformativeVirtualProps, target:Element): void;
 }
